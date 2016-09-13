e518427d6d73551091b51e454df46a414665532e
Jake Taemoon Jo <whxoans@gmail.com>
2016-09-13 15:38:50 +0900

    Initial Commit

---
+++ FlickrOpenFeedViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrOpenFeedViewer
+//
+//  Created by Jake Jo on 2016. 9. 13..
+//  Copyright © 2016년 Taemoon Jo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
